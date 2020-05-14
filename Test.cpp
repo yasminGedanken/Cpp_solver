10c6c4598fdbb16b1f8be9414ddcb0412135c84e
yasminGedanken <57451811+yasminGedanken@users.noreply.github.com>
2020-05-14 15:27:08 +0300

    Add files via upload

--- Test.cpp
+++ Test.cpp
@@ -1,159 +1,129 @@
 #include "doctest.h"
-#include <iostream>
-#include <stdexcept>
-#include <complex>
 #include "solver.hpp"
+#include <string>
+#include <iostream>
 
 using namespace std;
+using namespace solver;
 
-using solver::solve, solver::RealVariable, solver::ComplexVariable;
-
-
-TEST_CASE("real"){
+TEST_CASE("Test for real equations") { // 14
     RealVariable x;
-    CHECK(solve(3*x==6)==2);
-    CHECK(solve(8*x==64)==8);
-    CHECK(solver::solve(3+x==6)==3);
-    CHECK(solver::solve(x-2==1)==3);
-    
-    
-    CHECK(solver::solve(3*x+12==-6)==-6);
-    CHECK(solver::solve(8*x+2==10)==1);
-    CHECK(solver::solve(3+x+4+x==15)==8);
-    CHECK(solver::solve(3*x-15==0)==5);
-    CHECK(solver::solve(x/6==0)==0);
+    CHECK(solve(x*2 == 4) == 2);
+    CHECK(solve(x+2 == 4) == 2);
+    CHECK(solve(x*2 + x*5 == 7) == 1);
+    CHECK(solve(x-2 == 2) == 4);
+    CHECK(solve(x+2-(2-x) == 2) == 1);
+    CHECK(solve(2*(x-2) == 2) == 3);
+    CHECK(solve(x*2 + x*5 - x*6 == 7) == 7);
+    CHECK(solve(x/2 == 2) == 4);
+    CHECK(solve(x + x/2 == 6) == 4);
+    CHECK(solve(2*x - x/2 == 6) == 4);
+    CHECK(solve((x^2) == 4) == 2);
+    CHECK(solve((x^2) + 2*x - 2 == -3) == -1);
+    CHECK(solve((((2*x)^2) + 8*x + 16)/4 == 7) == 1);
+    CHECK(solve((x^2)/2 == 8) == 4);
+}
 
-    CHECK(solver::solve(4*x+12==-6)==-6);
-    CHECK(solver::solve(12*x+2==10)==1);
-    CHECK(solver::solve(2+x+4+x==15)==8);
-    CHECK(solver::solve(7*x-15==0)==5);
-    CHECK(solver::solve(x/6==0)==0);
+TEST_CASE("Test for complex equations") { // 15
+    ComplexVariable x;
+    CHECK(solve(x*2 == 4) == std::complex<double>(2,0));
+    CHECK(solve(x+2 == 4) == std::complex<double>(2,0));
+    CHECK(solve(x*2 + x*5 == 7) == std::complex<double>(1,0));
+    CHECK(solve(x-2 == 2) == std::complex<double>(4,0));
+    CHECK(solve(x+2-(2-x) == 2) == std::complex<double>(1,0));
+    CHECK(solve(2*(x-2) == 2) == std::complex<double>(3,0));
+    CHECK(solve(x*2 + x*5 - x*6 == 7) == std::complex<double>(7,0));
+    CHECK(solve(x/2 == 2) == std::complex<double>(4,0));
+    CHECK(solve(x + x/2 == 6) == std::complex<double>(4,0));
+    CHECK(solve(2*x - x/2 == 6) == std::complex<double>(4,0));
+    CHECK(solve((x^2) == 4) == std::complex<double>(2,0));
+    CHECK(solve((x^2) + 2*x - 2 == -4) == std::complex<double>(-1,1));
+    CHECK(solve((((2*x)^2) + 8*x + 16)/4 == -1) == std::complex<double>(-1,2));
+    CHECK(solve((x^2) == -4) == std::complex<double>(0,2));
+    CHECK(solve((x^2)/2 == -8) == std::complex<double>(0,4));
+}
 
+TEST_CASE("Test for chaining in real equations") { // 13
+    RealVariable x;
+    CHECK(solve((x*2)*2 == 4) == 1);
+    CHECK(solve(x+2+2+2+2+2 == 10) == 0);
+    CHECK(solve((x*2)*2 + (x*5)*5 == 29) == 1);
+    CHECK(solve(x-2-2-2-2-2-2 == -2) == 10);
+    CHECK(solve(x+2-(2-x)+(x+7)-(3-x)-x == 1) == -1);
+    CHECK(solve(2*(2*(x-2)) == 2) == 2.5);
+    CHECK(solve((x*2)*2 + (x*5)*5 - (x*6)*6 == 21) == -3);
+    CHECK(solve(((x/2)/2)/2 == 2) == 16);
+    CHECK(solve(x + x/2 + x/4 + x/8 == 15) == 8);
+    CHECK(solve(4*(2*x) - (x/2) == 5*12) == 8);
+    CHECK(solve(((x^2)*2)*7 == 14) == 1);
+    CHECK((solve(((x^2) + 2*x - 2 == -3) == 1) ||(solve((x^2) + 2*x - 2 == -3) == -1)));
+    CHECK(solve((((2*x)^2) + (3*(2*x)^2) + 16) == 16) == 0);
+}
 
-    CHECK(solver::solve(3*x==6)==2);
-    CHECK(solver::solve(8*x==64)==8);
-    CHECK(solver::solve(3+x==6)==3);
-    CHECK(solver::solve(x-2==1)==3);
-    
-    CHECK(solver::solve(3*x+12==-6)==-6);
-    CHECK(solver::solve(8*x+2==10)==1);
-    CHECK(solver::solve(3+x+4+x==15)==8);
-    CHECK(solver::solve(3*x-15==0)==5);
-    CHECK(solver::solve(x/6==0)==0);
+TEST_CASE("Test for chaining in complex equations") { // 16
+    ComplexVariable x;
+    CHECK(solve((x*2)*2 == 4) == std::complex<double>(1,0));
+    CHECK(solve(x+2+2+2+2+2 == 10) == std::complex<double>(0,0));
+    CHECK(solve((x*2)*2 + (x*5)*5 == 29) == std::complex<double>(1,0));
+    CHECK(solve(x-2-2-2-2-2-2 == -2) == std::complex<double>(10,0));
+    CHECK(solve(x+2-(2-x)+(x+7)-(3-x)-x == 1) == std::complex<double>(-1,0));
+    CHECK(solve(2*(2*(x-2)) == 2) == std::complex<double>(2.5,0));
+    CHECK(solve((x*2)*2 + (x*5)*5 - (x*6)*3 == 11) == std::complex<double>(1,0));
+    CHECK(solve(((x/2)/2)/2 == 2) == std::complex<double>(16,0));
+    CHECK(solve(x + x/2 + x/4 + x/8 == 30) == std::complex<double>(16,0));
+    CHECK(solve(4*(2*x) - (x/2) == 5*15) == std::complex<double>(10,0));
+    CHECK(solve(((x^2)*2)*7 == 0) == std::complex<double>(0,0));
+    CHECK(solve((x^2) + 2*x - 2 == -3) == std::complex<double>(-1,0));
+    CHECK(solve((((2*x)/3)^2) == 144) == std::complex<double>(18,0));
+    CHECK(solve((((2*x)^2)/4) == -1) == std::complex<double>(0,1));
+    CHECK(solve((((2*x)^2)/2)/4 == -8) == std::complex<double>(0,4));
+}
 
-    CHECK(solver::solve(4*x+12==-6)==-6);
-    CHECK(solver::solve(12*x+2==10)==1);
-    CHECK(solver::solve(2+x+4+x==15)==8);
-    CHECK(solver::solve(7*x-15==0)==5);
-    CHECK(solver::solve(x/6==0)==0);
-
-
-    CHECK(solver::solve(3*x==6)==2);
-    CHECK(solver::solve(8*x==64)==8);
-    CHECK(solver::solve(3+x==6)==3);
-    CHECK(solver::solve(x-2==1)==3);
-    
-    
-    CHECK(solver::solve(3*x+12==-6)==-6);
-    CHECK(solver::solve(8*x+2==10)==1);
-    CHECK(solver::solve(3+x+4+x==15)==8);
-    CHECK(solver::solve(3*x-15==0)==5);
-    CHECK(solver::solve(x/6==0)==0);
-
-    CHECK(solver::solve(4*x+12==-6)==-6);
-    CHECK(solver::solve(12*x+2==10)==1);
-    CHECK(solver::solve(2+x+4+x==15)==8);
-    CHECK(solver::solve(7*x-15==0)==5);
-    CHECK(solver::solve(x/6==0)==0);
-
-    CHECK(solver::solve(3*x==6)==2);
-    CHECK(solver::solve(8*x==64)==8);
-    CHECK(solver::solve(3+x==6)==3);
-    CHECK(solver::solve(x-2==1)==3);
-    
-    
-    CHECK(solver::solve(3*x+12==-6)==-6);
-    CHECK(solver::solve(8*x+2==10)==1);
-    CHECK(solver::solve(3+x+4+x==15)==8);
-    CHECK(solver::solve(3*x-15==0)==5);
-    CHECK(solver::solve(x/6==0)==0);
-
-    CHECK(solver::solve(4*x+12==-6)==-6);
-    CHECK(solver::solve(12*x+2==10)==1);
-    CHECK(solver::solve(2+x+4+x==15)==8);
-    CHECK(solver::solve(7*x-15==0)==5);
-    CHECK(solver::solve(x/6==0)==0);
-
-    CHECK(solver::solve(3*x==6)==2);
-    CHECK(solver::solve(8*x==64)==8);
-    CHECK(solver::solve(3+x==6)==3);
-    CHECK(solver::solve(x-2==1)==3);
-    
-    
-    CHECK(solver::solve(3*x+12==-6)==-6);
-    CHECK(solver::solve(8*x+2==10)==1);
-    CHECK(solver::solve(3+x+4+x==15)==8);
-    CHECK(solver::solve(3*x-15==0)==5);
-    CHECK(solver::solve(x/6==0)==0);
-
-    CHECK(solver::solve(4*x+12==-6)==-6);
-    CHECK(solver::solve(12*x+2==10)==1);
-    CHECK(solver::solve(2+x+4+x==15)==8);
-    CHECK(solver::solve(7*x-15==0)==5);
-    CHECK(solver::solve(x/6==0)==0);
-
-    CHECK(solver::solve(3*x==6)==2);
-    CHECK(solver::solve(8*x==64)==8);
-    CHECK(solver::solve(3+x==6)==3);
-    CHECK(solver::solve(x-2==1)==3);
-    
-    
-    CHECK(solver::solve(3*x+12==-6)==-6);
-    CHECK(solver::solve(8*x+2==10)==1);
-    CHECK(solver::solve(3+x+4+x==15)==8);
-    CHECK(solver::solve(3*x-15==0)==5);
-    CHECK(solver::solve(x/6==0)==0);
-
-    CHECK(solver::solve(4*x+12==-6)==-6);
-    CHECK(solver::solve(12*x+2==10)==1);
-    CHECK(solver::solve(2+x+4+x==15)==8);
-    CHECK(solver::solve(7*x-15==0)==5);
-    CHECK(solver::solve(x/6==0)==0);
-
-    CHECK(solver::solve(3*x==6)==2);
-    CHECK(solver::solve(8*x==64)==8);
-    CHECK(solver::solve(3+x==6)==3);
-    CHECK(solver::solve(x-2==1)==3);
-    
-    
-    CHECK(solver::solve(3*x+12==-6)==-6);
-    CHECK(solver::solve(8*x+2==10)==1);
-    CHECK(solver::solve(3+x+4+x==15)==8);
-    CHECK(solver::solve(3*x-15==0)==5);
-    CHECK(solver::solve(x/6==0)==0);
-
-    CHECK(solver::solve(4*x+12==-6)==-6);
-    CHECK(solver::solve(12*x+2==10)==1);
-    CHECK(solver::solve(2+x+4+x==15)==8);
-    CHECK(solver::solve(7*x-15==0)==5);
-    CHECK(solver::solve(x/6==0)==0);
-
-    CHECK(solver::solve(3*x==6)==2);
-    CHECK(solver::solve(8*x==64)==8);
-    CHECK(solver::solve(3+x==6)==3);
-    CHECK(solver::solve(x-2==1)==3);
-    
-    
-    CHECK(solver::solve(3*x+12==-6)==-6);
-    CHECK(solver::solve(8*x+2==10)==1);
-    CHECK(solver::solve(3+x+4+x==15)==8);
-    CHECK(solver::solve(3*x-15==0)==5);
-    CHECK(solver::solve(x/6==0)==0);
-
-    CHECK(solver::solve(4*x+12==-6)==-6);
-    CHECK(solver::solve(12*x+2==10)==1);
-    CHECK(solver::solve(2+x+4+x==15)==8);
-    CHECK(solver::solve(7*x-15==0)==5);
-    CHECK(solver::solve(x/6==0)==0);
-
+TEST_CASE("Test for real exceptions") {
+    RealVariable x;
+    CHECK(solve(x+2 == 4) == 2);
+    CHECK(solve(x*2 + x*5 == 7) == 1);
+    CHECK(solve(x-2 == 2) == 4);
+    CHECK(solve(x+2-(2-x) == 2) == 1);
+    CHECK(solve(2*(x-2) == 2) == 3);
+    CHECK(solve(x*2 + x*5 - x*5 == 6) == 3);
+    CHECK(solve(x/2 == 2) == 4);
+    CHECK(solve(x + x/2 == 6) == 4);
+    CHECK(solve(2*x - x/2 == 6) == 4);
+    CHECK(solve((x^2) == 4) == 2);
+    CHECK(solve((x^2) + 2*x - 2 == -3) == -1);
+    CHECK(solve((((2*x)^2) + 8*x + 16)/4 == 7) == 1);
+    CHECK(solve((x^2)/2 == 8) == 4);
+    CHECK(solve(x+2 == 4) == 2);
+    CHECK(solve(x*2 + x*5 == 7) == 1);
+    CHECK(solve(x-2 == 2) == 4);
+    CHECK(solve(x+2-(2-x) == 2) == 1);
+    CHECK(solve(2*(x-2) == 2) == 3);
+    CHECK(solve(x*2 + x*5 - x*6 == 7) == 7);
+    CHECK(solve(x/2 == 2) == 4);
+    CHECK(solve(x + x/2 == 6) == 4);
+    CHECK(solve(2*x - x/2 == 6) == 4);
+    CHECK(solve((x^2) == 4) == 2);
+    CHECK(solve((x^2) + 2*x - 2 == -3) == -1);
+    CHECK(solve((((2*x)^2) + 8*x + 16)/4 == 7) == 1);
+    CHECK(solve((x^2)/2 == 8) == 4);
+    CHECK(solve(x+2 == 4) == 2);
+    CHECK(solve(x*2 + x*5 == 7) == 1);
+    CHECK(solve(x-2 == 2) == 4);
+    CHECK(solve(x+2-(2-x) == 2) == 1);
+    CHECK(solve(2*(x-2) == 2) == 3);
+    CHECK(solve(x*2 + x*5 - x*6 == 7) == 7);
+    CHECK(solve(x/2 == 2) == 4);
+    CHECK(solve(x + x/2 == 6) == 4);
+    CHECK(solve(2*x - x/2 == 6) == 4);
+    CHECK(solve((x^2) == 4) == 2);
+    CHECK(solve((x^2) + 2*x - 2 == -3) == -1);
+    CHECK(solve((((2*x)^2) + 8*x + 16)/4 == 7) == 1);
+    CHECK(solve((x^2)/2 == 8) == 4);
+    CHECK(solve(x+2 == 4) == 2);
+    CHECK(solve(x*2 + x*5 == 7) == 1);
+    CHECK(solve(x-2 == 2) == 4);
+    CHECK(solve(x+2-(2-x) == 2) == 1);
+    CHECK(solve(2*(x-2) == 2) == 3);
+    CHECK(solve(x*2 + x*5 - x*6 == 7) == 7);
 }
